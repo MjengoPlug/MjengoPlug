96f982d527a8f7c9d4628d0e7cba97c80c4a0ef5
Chris <mwangic106@gmail.com>
2025-03-08 23:50:58 +0300

    created custom user

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ('email', 'user_name', 'first_name',)
+    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'user_name', 'first_name',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+    
+admin.site.register(CustomUser, UserAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+    
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class CustomUser(AbstractUser,PermissionError):
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    phone_number=models.CharField(max_length=150, blank=True,unique=True)
+    role=models.CharField(max_length=150, blank=True)
+
+
+    objects=UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name','first_name']
+
+    
+
+
